618611a9097d11d2996e790848b437105331ce00
Adam James <adam.james870@yahoo.co.uk>
2025-01-04 09:41:10 +0800

    Good b/d for picker and camera

--- PicSender/ViewModels/MainPageViewModel.cs
+++ PicSender/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using CommunityToolkit.Mvvm.Input;
 using PicSender.Models;
 
@@ -7,31 +8,63 @@ namespace PicSender.ViewModels;
 public partial class MainPageViewModel : BaseViewModel
 {
     public ObservableCollection<PicToSend> Pics { get; } = [];
+
+    private IMediaPicker _mediaPicker;
     
-    public MainPageViewModel()
+    public MainPageViewModel(IMediaPicker mediaPicker)
     {
+        _mediaPicker = mediaPicker;
         Pics.Add(new PicToSend { Name = "Pic 1" });
         Pics.Add(new PicToSend { Name = "Pic 2" });
         Pics.Add(new PicToSend { Name = "Pic 3" });
     }
 
     [RelayCommand]
-    async Task AddPictureAsync()
+    async Task PickPictureAsync()
     {
-        
-        var picture = await MediaPicker.CapturePhotoAsync();
-        if (picture is null) return;
-        Pics.Add(new PicToSend { Name = picture.FullPath });
-        
+
+        try
+        {
+            var picture = await _mediaPicker.PickPhotoAsync();
+            if (picture is null) return;
+            Pics.Add(new PicToSend { Name = picture.FullPath });
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error", $"Exception: {ex.Message}", "OK");
+        }
+        finally
+        {
+            
+        }
+
     }
     
     [RelayCommand]
     async Task TakePictureAsync()
     {
 
-        var picture = await MediaPicker.CapturePhotoAsync();
-        if (picture is null) return;
-        Pics.Add(new PicToSend { Name = picture.FullPath });
+        try
+        {
+           if (!_mediaPicker.IsCaptureSupported)
+           {
+               await Shell.Current.DisplayAlert("Error", "Capture is not supported on this device", "OK");
+               return;
+           }
+           var picture = await _mediaPicker.CapturePhotoAsync();
+           if (picture is null) return;
+           Pics.Add(new PicToSend { Name = picture.FullPath }); 
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error", $"Exception: {ex.Message}", "OK");
+        }
+        finally
+        {
+            
+        }
 
     }
     
